41456337cab101af209f15114f8965df5a0be052
Simran <itssimran903@gmail.com>
2024-03-17 02:18:45 +0530

    carousel component done

--- movix/src/pages/home/trending/Trending.jsx
+++ movix/src/pages/home/trending/Trending.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
 import useFetch from '../../../hooks/useFetch';
+import Carousel from '../../../components/carousel/Carousel';
+
 
 const Trending = () => {
     const [endpoint, setEndpoint] = useState("day")
@@ -11,14 +13,13 @@ const Trending = () => {
         setEndpoint(tab === 'Day' ? 'day' : 'week')
     }
 
-
     return (
         <div className="carouselSection">
             <ContentWrapper>
                 <span className="carouselTitle">Trending</span>
                 <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
             </ContentWrapper>
-            {/* <Carousel data={data?.results} loading={loading} /> */}
+            <Carousel data={data?.results} loading={loading} />
         </div>
     );
 }
