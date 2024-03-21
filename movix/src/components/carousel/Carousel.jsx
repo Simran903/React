8012843bd87919ad3ae9d271d00c88b9a984e7d8
Simran <itssimran903@gmail.com>
2024-03-22 01:08:34 +0530

    commit

--- movix/src/components/carousel/Carousel.jsx
+++ movix/src/components/carousel/Carousel.jsx
@@ -10,23 +10,28 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import CircleRating from "../circleRating/CircleRating";
-import "./style.scss";
 import Genres from "../genres/Genres";
+import "./style.scss";
 
+const Carousel = ({ data, loading, endpoint, title }) => {
+    const carouselContainer = useRef();
+    const { url } = useSelector((state) => state.home);
+    const navigate = useNavigate();
 
-const Carousel = ({ data, loading, endpoint, title}) => {
-    const carouselContainer = useRef()
-    const { url } = useSelector((state => state.home))
-    const navigate = useNavigate()
     const navigation = (dir) => {
-        const container = carouselContainer.current
-        const scrollAmount = dir === "left" ? container.scrollLeft - (container.offsetWidth + 20) : container.scrollLeft + (container.offsetWidth + 20)
+        const container = carouselContainer.current;
+
+        const scrollAmount =
+            dir === "left"
+                ? container.scrollLeft - (container.offsetWidth + 20)
+                : container.scrollLeft + (container.offsetWidth + 20);
 
         container.scrollTo({
             left: scrollAmount,
             behavior: "smooth",
-        })
-    }
+        });
+    };
+
     const skItem = () => {
         return (
             <div className="skeletonItem">
@@ -39,12 +44,11 @@ const Carousel = ({ data, loading, endpoint, title}) => {
         );
     };
 
-
     return (
         <div className="carousel">
             <ContentWrapper>
                 {title && <div className="carouselTitle">{title}</div>}
-            <BsFillArrowLeftCircleFill
+                <BsFillArrowLeftCircleFill
                     className="carouselLeftNav arrow"
                     onClick={() => navigation("left")}
                 />
@@ -55,26 +59,43 @@ const Carousel = ({ data, loading, endpoint, title}) => {
                 {!loading ? (
                     <div className="carouselItems" ref={carouselContainer}>
                         {data?.map((item) => {
-                            const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback
+                            const posterUrl = item.poster_path
+                                ? url.poster + item.poster_path
+                                : PosterFallback;
                             return (
                                 <div
-                                key={item.id}
-                                className="carouselItem" onClick={() => navigate(`${item.media_type || endpoint}/${item.id}`)}>
+                                    key={item.id}
+                                    className="carouselItem"
+                                    onClick={() =>
+                                        navigate(
+                                            `/${item.media_type || endpoint}/${item.id
+                                            }`
+                                        )
+                                    }
+                                >
                                     <div className="posterBlock">
-                                        <Img src={posterUrl}/>
-                                        <CircleRating rating={item.vote_average.toFixed(1)}/>
-                                        <Genres data={item.genre_ids.slice(0,2)}/>
+                                        <Img src={posterUrl} />
+                                        <CircleRating
+                                            rating={item.vote_average.toFixed(
+                                                1
+                                            )}
+                                        />
+                                        <Genres
+                                            data={item.genre_ids.slice(0, 2)}
+                                        />
                                     </div>
                                     <div className="textBlock">
                                         <span className="title">
-                                             {item.title || item.name}
+                                            {item.title || item.name}
                                         </span>
                                         <span className="date">
-                                             {dayjs(item.release_Date).format('MMM D, YYYY')}
+                                            {dayjs(item.release_date || item.first_air_date).format(
+                                                "MMM D, YYYY"
+                                            )}
                                         </span>
                                     </div>
                                 </div>
-                            )
+                            );
                         })}
                     </div>
                 ) : (
@@ -88,7 +109,7 @@ const Carousel = ({ data, loading, endpoint, title}) => {
                 )}
             </ContentWrapper>
         </div>
-    )
-}
+    );
+};
 
-export default Carousel
+export default Carousel;
